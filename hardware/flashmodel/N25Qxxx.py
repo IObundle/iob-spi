74d7a5ae71f421d5a6bb487287671157e089da36
Pedro Antunes <pedronmantunes@gmail.com>
2023-08-17 11:13:46 +0100

    flashmodel: create python setup.

---
+++ hardware/flashmodel/N25Qxxx.py
@@ -0,0 +1,15 @@
+import os
+
+from iob_module import iob_module
+
+
+class iob_merge(iob_module):
+    name = "N25Qxxx"
+    version = "V0.10"
+    flows = "sim"
+    setup_dir = os.path.dirname(__file__)
+
+    @classmethod
+    def _create_submodules_list(cls):
+        """Create submodules list with dependencies of this module"""
+        super()._create_submodules_list([ ])
