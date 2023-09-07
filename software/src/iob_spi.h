361ccb2c2a65184bbacfd03f65c136a93938058d
Pedro Antunes <pedronmantunes@gmail.com>
2023-09-07 14:29:15 +0100

    software/src/*: format all with clang; make a few adaptation to new firmware.

--- software/src/iob_spi.h
+++ software/src/iob_spi.h
@@ -1,4 +1,4 @@
-//Higher level functions
+// Higher level functions
 
 unsigned int spiflash_readStatusReg(unsigned *reg);
 unsigned int spiflash_readVolConfigReg(unsigned *regvalue);
@@ -7,12 +7,12 @@ void spiflash_RecoverSequence();
 
 void enterSPImode(int);
 
-//xip functions
+// xip functions
 int spiflash_XipEnable();
 int spiflash_terminateXipSequence();
 unsigned int spiflash_readMemXip(unsigned address, unsigned activateXip);
 
-//Reset
+// Reset
 void spiflash_resetmem();
 void spiflash_writemem(unsigned int word, unsigned int address);
 unsigned int spiflash_readmem(unsigned int address);
@@ -21,23 +21,26 @@ unsigned int spiflash_readmem(unsigned int address);
 void spiflash_erase_subsector(unsigned int subsector_address);
 void spiflash_erase_sector(unsigned int sector_address);
 
-//Fast Read functions
+// Fast Read functions
 unsigned int spiflash_readFlashParam(unsigned address);
-unsigned int spiflash_readfastDualOutput(unsigned address, unsigned activateXip);
-unsigned int spiflash_readfastQuadOutput(unsigned address, unsigned activateXip);
-unsigned int spiflash_readfastDualInOutput(unsigned address, unsigned activateXip);
-unsigned int spiflash_readfastQuadInOutput(unsigned address, unsigned activateXip);
+unsigned int spiflash_readfastDualOutput(unsigned address,
+                                         unsigned activateXip);
+unsigned int spiflash_readfastQuadOutput(unsigned address,
+                                         unsigned activateXip);
+unsigned int spiflash_readfastDualInOutput(unsigned address,
+                                           unsigned activateXip);
+unsigned int spiflash_readfastQuadInOutput(unsigned address,
+                                           unsigned activateXip);
 
-//Fast Read DTR functions
+// Fast Read DTR functions
 unsigned int spiflash_readfastDTR(unsigned address);
 unsigned int spiflash_readfastDualOutDTR(unsigned address);
 unsigned int spiflash_readfastDualIODTR(unsigned address);
 unsigned int spiflash_readfastQuadOutDTR(unsigned address);
 unsigned int spiflash_readfastQuadIODTR(unsigned address);
 
-
-//Memory Program functions
-int spiflash_memProgram(char* mem, int memsize, unsigned int address);
+// Memory Program functions
+int spiflash_memProgram(char *mem, int memsize, unsigned int address);
 
 void spiflash_programfastDualInput(unsigned int word, unsigned address);
 void spiflash_programfastDualInputExt(unsigned int word, unsigned address);
